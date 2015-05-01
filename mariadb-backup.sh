6f389a9f07156ced90fd80f5e8934de1d0c78462
bstillman <bstillman@gmail.com>
2015-05-01 13:06:12 -0400

    Fixes

--- mariadb-backup.sh
+++ mariadb-backup.sh
@@ -3,24 +3,35 @@
 ############################################
 # Config
 
-fullbackday=Wednesday				# Day of week to do full backup
-keepweek=4					# Number of weeks worth of backups to keep
-backupdir=/backups				# Full path to backup directory root
-logpath=/var/log                      		# Path to keep logs
-cryptkey=/etc/my.cnf.d/backupscript.key		# Full path to encryption key
-threads=4					# Used for parallel and compress
-encrypt=yes					# To be used later
-compress=yes					# To be used later
-maillist=ben@mariadb.com			# Comma separated list of email address to be notified.
-mailsubpre="[dbhc-mariadb]"			# Email subject prefix
+backupuser=testuser                      # MySQL backup username
+backuppass=testpass                      # MySQL backup user password
+monyog=no                                # If server monitored by MONyog, should we disable alerts?
+monyogserver=                            # The name of the server as setup in MONyog
+monyoguser=                              # MONyog username
+monyogpass=                              # MONyog password
+monyoghost=                              # MONyog host/ip
+monyogport=                              # MONyog port
+fullbackday=Friday                       # Day of week to do full backup
+keepweek=4                               # Number of weeks worth of backups to keep
+backupdir=/backups                       # Full path to backup directory root
+logpath=/var/log                         # Path to keep logs
+threads=4                                # Used for parallel and compress
+parallel=yes                             # Use parallel threads for backup?
+encrypt=yes                              # Encrypt backup?
+cryptkey=/etc/my.cnf.d/backupscript.key  # Full path to encryption key
+compress=yes                             # Compress backup?
+galera=no                                # Include Galera info?
+slave=no                                 # Include slave info? 
+maillist=ben@mariadb.com                 # Comma separated list of email address to be notified.
+mailsubpre="[dbhc-mariadb]"              # Email subject prefix
 
 ############################################
+
 # Functions
 
 # Mail function
 function mail_log {
 	mail -s "$mailsubpre $HOSTNAME Backup $log_status $mdate" $maillist < "$logfile"
-	insert_db_stats
 }
 
 # Function to check log for okay
@@ -32,116 +43,116 @@ function log_check {
 	fi
 }
 
-# Function to find or create backup base directory
-function backer_upper {
-	if [ "$(date +%A)" = $fullbackday ] && [ ! -d $backupdir/weekof"$(date +%m%d%y)" ]; then
-		log_info "Creating backup base directory."
-		mkdir -p $backupdir/weekof"$(date +%m%d%y)"
-		backupbasedir=$backupdir/weekof"$(date +%m%d%y)"
-		full_backup
-	elif [ "$(date +%A)" = $fullbackday ] && [ -d $backupdir/weekof"$(date +%m%d%y)" ] ; then
-		log_info "It is the day scheduled for full backups, but the weekly folder"
-		log_info "already exists. Assuming incremental backup. Check config."
-		log_info "Setting backup base directory variable."
-		backupbasedir=$backupdir/weekof"$(date +%m%d%y)"
-		incremental_backup
-	elif [ "$(date +%A)" != $fullbackday ] && [ ! -d $backupdir/weekof"$(date +%m%d%y)" ] ; then
-		log_info "It is an incremental backup day, however the weekly folder does"
-		log_info "not appear to exist. Please check config."
-		log_status=FAILED
-		mail_log
-		exit
-	else
-		log_info "Setting backup base directory variable."
-		backupbasedir=$backupdir/weekof"$(date -dlast-$fullbackday +%m%d%y)"
-		incremental_backup
-	fi
-}
-
-# Full backup function
-function full_backup {
-	log_info "Full backup beginning."
-	budirdate=$(date +%Y-%m-%d)
-	$innobackupex --galera-info --parallel=$threads --compress --compress-threads=$threads --encrypt=AES256 --encrypt-key-file=$cryptkey "$backupbasedir" 2>> "$logfile"
-	log_check
-	log_info "Full backup" $log_status
-	log_info "CAUTION: ALWAYS VERIFY YOUR BACKUPS."
-	butype=full
-	files=("$backupbasedir"/"$budirdate"*); budir=${files[${#files[@]} -1 ]}
-	xbcrypt -d --encrypt-key-file=$cryptkey --encrypt-algo=AES256 < "$budir"/xtrabackup_checkpoints.xbcrypt > "$budir"/xtrabackup_checkpoints
-	backup_cleanup
-	mail_log
-}
-
-# Incremental backup function
-function incremental_backup {
-	log_info "Incremental backup beginning."
-	budirdate=$(date +%Y-%m-%d)
-	incrbase=$backupbasedir/$(ls -tr "$backupbasedir" | tail -1)
-	$innobackupex --galera-info --parallel=$threads --compress --compress-threads=$threads --encrypt=AES256 --encrypt-key-file=$cryptkey --incremental "$backupbasedir" --incremental-basedir="$incrbase" 2>> "$logfile"
-	log_check
-	log_info "Incremental backup" $log_status
-	log_info "CAUTION: ALWAYS VERIFY YOUR BACKUPS."
-	butype=incremental
-	files=("$backupbasedir"/"$budirdate"*); budir=${files[${#files[@]} -1 ]}
-	xbcrypt -d --encrypt-key-file=$cryptkey --encrypt-algo=AES256 < "$budir"/xtrabackup_checkpoints.xbcrypt > "$budir"/xtrabackup_checkpoints
-	backup_cleanup
-	mail_log
-}
-
-# Function to cleanup old backup directories. This is definitely not the best way.
-function backup_cleanup {
-	if [ $log_status = SUCCEEDED ] ; then
-		tailnum=$(($keepweek+1))
-		declare -a TO_DELETE=($(ls -ar "$backupdir" | grep weekof | tail -n +"$tailnum"))
-		if [ ${#TO_DELETE[@]} -gt 1 ] ; then
-			log_info "Beginning cleanup of old weekly backup directories."
-			for d in "${TO_DELETE[@]}"
-			do
-				log_info "Deleted backup directory $d"
-				rm -Rf "${backupdir}"/"$d"
-			done
-			log_info "Backup directory cleanup complete."
-		else
-			log_info "No backup directories to clean."
-		fi
-	else
-		log_info "Backup failed. No old backups deleted at this time."
-	fi
-}
-
-# Function to insert info into database table for MONyog monitoring
-# use dbmon group suffix --defaults-group-suffix=dbmon
-function insert_db_stats {
-	source_host=$(hostname)
-	end_time=$(date +'%T')
-	backup_size="$(du -sm "$budir" | awk '{print $1}')"
-	debugme
-	mysql --defaults-group-suffix=backupmon -e "INSERT INTO backupmon.backups (created_date, start_time, end_time, source_host, location, type, threads, encrypted, compressed, size, status, mailed_to, log_file) VALUES ('$created_date', '$start_time', '$end_time', '$source_host', '$budir', '$butype', '$threads', '$encrypt', '$compress', '$backup_size', '$log_status', '$maillist', '$logfile');"
-}
-
 # Logging function
 function log_info() {
 	printf "%s --> %s\n" "$(date +%Y-%m-%d-%T)" "$*" >>"$logfile";
 }
 
+# Function to create innobackupex command
+function innocreate {
+	mhost=$(hostname)
+	innocommand="$innobackupex $backupdir --history=$mhost"
+	if [ "$(date +%A)" = $fullbackday ] ; then
+		butype=Full
+	else
+		butype=Incremental
+		innocommand=$innocommand" --incremental --incremental-history-name=$mhost"
+	fi
+	[ ! -z "$backupuser" ] && innocommand=$innocommand" --user=$backupuser"
+	[ ! -z "$backuppass" ] && innocommand=$innocommand" --password=$backuppass"
+	if [ "$galera" = yes ] ; then innocommand=$innocommand" --galera-info" ; fi
+	if [ "$slave" = yes ] ; then innocommand=$innocommand" --slave-info" ; fi
+	if [ "$parallel" = yes ] ; then innocommand=$innocommand" --parallel=$threads" ; fi
+	if [ "$compress" = yes ] ; then innocommand=$innocommand" --compress --compress-threads=$threads" ; fi
+	if [ "$encrypt" = yes ] ; then innocommand=$innocommand" --encrypt=AES256 --encrypt-key-file=$cryptkey" ; fi
+}
+
+# Function to decrypt xtrabackup_checkpoints 
+function checkpointsdecrypt {
+	budirdate=$(date +%Y-%m-%d)
+	files=("$backupdir"/"$budirdate"*); budir=${files[${#files[@]} -1 ]}
+	xbcrypt -d --encrypt-key-file=$cryptkey --encrypt-algo=AES256 < "$budir"/xtrabackup_checkpoints.xbcrypt > "$budir"/xtrabackup_checkpoints
+}
+
+# Function to disable/enable MONyog alerts
+function monyog {
+	curl "${monyoghost}:${monyogport}/?_object=MONyogAPI&_action=Alerts&_value=${1}&_user=${monyoguser}&_password=${monyogpass}&_server=${monyogserver}"
+}
+
+# Function to do the backup
+function backer_upper {
+	innocreate
+	if [ "$monyog" = yes ] ; then
+		log_info "Disabling MONyog alerts"
+		monyog disable
+		sleep 30
+	fi
+	if [ "$galera" = yes ] ; then
+		log_info "Enabling WSREP desync."
+		mysql -u $backupuser -p $backuppass -e "SET GLOBAL wsrep_desync=ON;"
+	fi
+	log_info "Beginning ${butype} Backup"
+	$innocommand 2>> "$logfile"
+	if [ "$encrypt" = yes ] ; then 
+		checkpointsdecrypt
+	fi
+	if [ "$galera" = yes ] ; then
+		log_info "Disabling WSREP desync."
+		# wsrep_local_recv_queue
+		until [ "$queue" -eq 0 ]; do
+    		queue=$(mysql -u $backupuser -p $backuppass -ss -e "show global status like 'wsrep_local_recv_queue';" | awk '{ print $2 }')
+    		sleep 10
+		done
+		mysql -u $backupuser -p $backuppass -e "SET GLOBAL wsrep_desync=OFF;"
+	fi
+	if [ "$monyog" = yes ] ; then
+		log_info "Enabling MONyog alerts"
+		monyog enable
+		sleep 30
+	fi
+	log_check
+	log_info "$butype backup $log_status"
+	log_info "CAUTION: ALWAYS VERIFY YOUR BACKUPS."
+}
+
+# Function to cleanup old backups.
+function backup_cleanup {
+	#...
+	true
+}
 
 # Debug variables function
 function debugme {
-	echo "created_date: " "$created_date"
-	echo "start_time: " "$start_time"
-	echo "end_time: " "$end_time"
-	echo "source_host: " "$source_host"
-	echo "budirdate: " "$budirdate"
-	echo "budir: " "$budir"
-	echo "butype: " "$butype"
+	echo "backupuser: " "$backupuser"
+	echo "backuppass: " "$backuppass"
+	echo "monyog: " "$monyog"
+	echo "monyogserver: " "$monyogserver"
+	echo "monyoguser: " "$monyoguser"
+	echo "monyogpass: " "$monyogpass"
+	echo "monyoghost: " "$monyoghost"
+	echo "monyogport: " "$monyogport"
+	echo "fullbackday: " "$fullbackday"
+	echo "keepweek: " "$keepweek"
+	echo "backupdir: " "$backupdir"
+	echo "logpath: " "$logpath"
 	echo "threads: " "$threads"
+	echo "parallel: " "$parallel"
 	echo "encrypt: " "$encrypt"
+	echo "cryptkey: " "$cryptkey"
 	echo "compress: " "$compress"
-	echo "backup_size: " "$backup_size"
-	echo "log_status: " "$log_status"
+	echo "galera: " "$galera"
+	echo "slave: " "$slave"
 	echo "maillist: " "$maillist"
+	echo "mailsubpre: " "$mailsubpre"
+	echo "mdate: " "$mdate"
 	echo "logfile: " "$logfile"
+	echo "queue: " "$queue"
+	echo "butype: " "$butype"
+	echo "log_status: " "$log_status"
+	echo "budirdate: " "$budirdate"	
+	echo "innocommand: " "$innocommand"
+	echo "mhost: " "$mhost"
+	echo "budir: " "$budir"
 }
 
 ############################################
@@ -150,13 +161,6 @@ function debugme {
 # Set some specific variables
 mdate=$(date +%m/%d/%y)	# Date for mail subject. Not in function so set at script start time, not when backup is finished.
 logfile=$logpath/mariadb_backup_$(date +%Y-%m-%d-%T).log	# logfile
-created_date=$(date +'%F')
-start_time=$(date +'%T')
-
-# Create log file
-touch "$logfile"
-
-log_info "Backup of" "$(hostname)" "beginning."
 
 # Check for xtrabackup
 if command -v innobackupex >/dev/null; then
@@ -168,6 +172,12 @@ else
    exit
 fi
 
-backer_upper
+backer_upper # Execute the backup. 
+
+backup_cleanup # Cleanup old backups. 
+
+mail_log # Mail results to maillist.
+
+debugme	# Comment out to disable listing of all variables.
 
 exit
